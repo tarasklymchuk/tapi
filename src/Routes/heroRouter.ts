89c74bc764e843278c6c0a6923fdcdc7fceb5ccc
taras.klymchuk <taras.klymchuk@gmail.com>
2018-06-19 00:03:42 +0300

    update hero route

--- src/Routes/heroRouter.ts
+++ src/Routes/heroRouter.ts
@@ -1,50 +1,60 @@
 import {Router, Request, Response, NextFunction} from 'express';
+
 const Heroes = require('../../Data/heroes.json');
 
 export class HeroRouter {
-  router: Router
+    router: Router
 
-  /**
-   * Initialize the HeroRouter
-   */
-  constructor() {
-    this.router = Router();
-    this.init();
-  }
+    /**
+     * Initialize the HeroRouter
+     */
+    constructor() {
+        this.router = Router();
+        this.init();
+    }
 
-  /**
-   * GET all Heroes.
-   */
-  public getAll(req: Request, res: Response, next: NextFunction) {
-    res.send(Heroes);
-  }
-public getOne(req: Request, res: Response, next: NextFunction) {
-  let query = parseInt(req.params.id);
-  let hero = Heroes.find(hero => hero.id === query);
-  if (hero) {
-    res.status(200)
-      .send({
-        message: 'Success',
-        status: res.status,
-        hero
-      });
-  }
-  else {
-    res.status(404)
-      .send({
-        message: 'No hero found with the given id.',
-        status: res.status
-      });
-  }
-}
-  /**
-   * Take each handler, and attach to one of the Express.Router's
-   * endpoints.
-   */
-  init() {
-    this.router.get('/', this.getAll);
-    this.router.get('/:id', this.getOne);
-  }
+    /**
+     * GET all Heroes.
+     */
+    public getAll(req: Request, res: Response, next: NextFunction) {
+        res.send(Heroes);
+    }
+
+    /**
+     * get one hero
+     *
+     * @param {e.Request} req
+     * @param {e.Response} res
+     * @param {e.NextFunction} next
+     */
+    public getOne(req: Request, res: Response, next: NextFunction) {
+        let query = parseInt(req.params.id);
+        let hero = Heroes.find(hero => hero.id === query);
+        if (hero) {
+            res.status(200)
+                .send({
+                    message: 'Success',
+                    status: res.status,
+                    hero
+                });
+        }
+        else {
+            res.status(404)
+                .send({
+                    message: 'No hero found with the given id.',
+                    status: res.status
+                });
+        }
+    }
+
+    /**
+     * Take each handler, and attach to one of the Express.Router's
+     * endpoints.
+     */
+    init() {
+        this.router.get('/', this.getAll);
+        this.router.get('/:id', this.getOne);
+    }
 
 }
 
