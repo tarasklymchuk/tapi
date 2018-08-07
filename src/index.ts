db82b8ca8221f4a9ac6ac232b0890a9a2bc32528
Taras Klymchuk <taras.klymchuk@lemberg.co.uk>
2018-08-07 17:33:49 +0300

    update application run

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import App from "./App";
  * Setup routing-controllers to use typedi container.
  */
 useContainer(Container);
-
+const port = normalizePort(process.env.PORT || 3000);
 /**
  * We create a new express server instance.
  * We could have also use useExpressServer here to attach controllers to an existing express instance.
@@ -17,7 +17,38 @@ const Application = createExpressServer(App);
 /**
  * Start the express app.
  */
-Application.listen(3000);
-console.log("Server is up and running at port 3000");
+Application.listen(port);
+Application.on('error', onError);
+Application.on('listening', onListening);
+
+function normalizePort(val: number | string): number | string | boolean {
+    let port: number = (typeof val === 'string') ? parseInt(val, 10) : val;
+    if (isNaN(port)) return val;
+    else if (port >= 0) return port;
+    else return false;
+}
+
+function onError(error: NodeJS.ErrnoException): void {
+    if (error.syscall !== 'listen') throw error;
+    let bind = (typeof port === 'string') ? 'Pipe ' + port : 'Port ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(`${bind} requires elevated privileges`);
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(`${bind} is already in use`);
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening(): void {
+    let addr = Application.address();
+    let bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
+    console.log(`Server is up and running at ${bind}`);
+}
 
 module.exports = Application;
